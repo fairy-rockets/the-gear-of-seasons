ad9f009fd2664e7abdac20c331f84732a96b09c5
psi <psi@7io.org>
2018-04-25 18:07:19 +0900

    gulp

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,18 @@
 import Gear from './Gear';
 
 function main() {
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.getElementById('background');
+  const ctx = canvas.getContext('webgl');
+  if(ctx) {
+    const parent =canvas.parentElement;
+    paret.removeChild(canvas);
+    const elem = document.createElement('div');
+    elem.textContent='WebGL not supported';
+    elem.id = 'background';
+    parent.appendChild(elem);
+    return;
+  }
   const gear = new Gear();
 }
 
