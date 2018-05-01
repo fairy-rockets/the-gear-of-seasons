ec20cb29ecfdce95fe0e479aa7e8a0fd112d79ce
psi <psi@7io.org>
2018-05-02 02:37:11 +0900

    background air

--- frontend/actors/Background.js
+++ frontend/actors/Background.js
@@ -87,12 +87,53 @@ uniform float time;
 varying vec2 vPosition;
 
 // https://stackoverflow.com/questions/4200224/random-noise-functions-for-glsl
-float rand(vec2 co){
+float rand(vec2 co) {
   return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
 }
 
+// from https://wgld.org/d/webgl/w025.html
+const int   oct  = 8;
+const float per  = 0.5;
+const float PI   = 3.1415926;
+
+// 補間関数
+float interpolate(float a, float b, float x){
+	float f = (1.0 - cos(x * PI)) * 0.5;
+	return a * (1.0 - f) + b * f;
+}
+
+// 補間乱数
+float irnd(vec2 p){
+	vec2 i = floor(p);
+	vec2 f = fract(p);
+	vec4 v = vec4(
+          rand(vec2(i.x,       i.y      )),
+				  rand(vec2(i.x + 1.0, i.y      )),
+				  rand(vec2(i.x,       i.y + 1.0)),
+				  rand(vec2(i.x + 1.0, i.y + 1.0)));
+	return interpolate(interpolate(v.x, v.y, f.x), interpolate(v.z, v.w, f.x), f.y);
+}
+
+// ノイズ生成
+float noise(vec2 p){
+	float t = 0.0;
+	for(int i = 0; i < oct; i++){
+		float freq = pow(2.0, float(i));
+		float amp  = pow(per, float(oct - i));
+		t += irnd(vec2(p.x / freq, p.y / freq)) * amp;
+	}
+	return t;
+}
+
 void main(void) {
-  float n = 0.2;
-  gl_FragColor = vec4(n, n, n, 1);
+  float sum = 0.0;
+
+  float n = noise(gl_FragCoord.xy + vec2(30000.0 - time / 80.0, 30000.0 - time / 60.0));
+  vec3 cloud1 = vec3(pow(n, 3.0) * 0.7);
+
+  n = noise(gl_FragCoord.xy + vec2(-time / 150.0, time / 120.0));
+  vec3 cloud2 = vec3(45.0/255.0, 60.0/355.0, 109.0/255.0) * pow(n, 1.0) * 0.3;
+
+  gl_FragColor = vec4(cloud1 + cloud2, 1.0);
 }
 `;
