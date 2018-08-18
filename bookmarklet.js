3998c9ad666a341b65c9632b61bd18f17f1bb3d5
psi <psi@7io.org>
2018-08-18 21:51:54 +0900

    bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,13 @@
+(function(){
+    const host = location.host;
+    const href = location.href;
+    if(host === "hexe.net") {
+        location.href = href.replace(host, "ura.hexe.net");
+    }else if(host === "ura.hexe.net") {
+        location.href = href.replace(host, "hexe.net");
+    }else{
+        location.href = "https://hexe.net/";
+    }
+})();
+
+// use https://mrcoles.com/bookmarklet/ to make bookmarklet.
