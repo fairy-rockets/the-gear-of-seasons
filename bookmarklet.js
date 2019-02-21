9296e52b90ee33b14257ff69e05d512813faa4c5
psi <psi@7io.org>
2019-02-21 16:26:56 +0900

    ブックマークレット更新

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,10 @@
 (function(){
     const host = location.host;
     const href = location.href;
-    if(host === "hexe.net") {
-        location.href = href.replace(host, "ura.hexe.net");
-    }else if(host === "ura.hexe.net") {
-        location.href = href.replace(host, "hexe.net");
+    if(host === 'hexe.net') {
+        location.href = href.replace('http://hexe.net/', 'https://ura.hexe.net/');
+    }else if(host === 'ura.hexe.net') {
+        location.href = href.replace('https://ura.hexe.net/', "http://hexe.net/");
     }else{
         location.href = "http://hexe.net/";
     }
