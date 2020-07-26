776bc5ebd944ec3da9714a3e5ab8d95e5253bffe
Kaede Fujisaki <kaede@hexe.net>
2020-07-27 07:59:57 +0900

    use typescript in webpack confg & gulpfile

--- gulpfile.ts
+++ gulpfile.ts
@@ -6,24 +6,20 @@ import colors from 'ansi-colors';
 import webpackStream from 'webpack-stream';
 import webpack from 'webpack';
 
-import child, {ChildProcess} from 'child_process';
-
-import webpackConfig from './webpack.config.js';
+import child from 'child_process';
+import webpackConfig from "./webpack.config";
 
 const ServerPath = 'github.com/fairy-rockets/the-gear-of-seasons/cmd/the-gear-of-seasons';
 const ServerBin  = '.bin/the-gear-of-seasons';
 
 async function buildClient() {
-  const stream = webpackStream(webpackConfig, webpack)
+  const stream = webpackStream(webpackConfig, webpack.prototype)
           .on('error', function(err) {
             log(colors.red(err));
-            this.emit('end');
+            gulp.emit('end');
            })
           .pipe(gulp.dest('_resources/static'));
-  /**
-   * @type {boolean} result
-   */
-  const result = await new Promise((resolve, reject) => {
+  const result: boolean = await new Promise((resolve, reject) => {
     stream.on('finish', () => resolve(true)).on('error', reject);
   });
   return result;
@@ -31,17 +27,12 @@ async function buildClient() {
 
 gulp.task('client:build', buildClient);
 
-/**
- * @param {string[]} args 
- * @param {child.SpawnOptions} [options]
- * @returns {Promise<string>}
- */
-function exec(args, options) {
+function exec(args: string[], options: child.SpawnOptions): Promise<string> {
   return new Promise((resolve, reject) => {
     log(`Spawn % ${args.map((s) => s.indexOf(' ') < 0 ? s : `"${s}"`).join(' ')}`);
-    const cmd = args.shift();
+    const cmd: string = args.shift()!;
     const p = child.spawn(cmd, args, options);
-    p.stderr.on('data', (err) => {
+    p.stderr!.on('data', (err) => {
       log(colors.red(`Error (${cmd}): ${err}`))
     });
     p.once('exit', (code, signal) => {
@@ -51,15 +42,8 @@ function exec(args, options) {
   });
 }
 
-/**
- * @param {string} dst
- * @param {string} [os] 
- * @param {string} [arch] 
- * @returns {Promise<string>}
- */
-async function buildServer(dst, os, arch) {
-  /** @type {child.SpawnOptions} */
-  const options = {};
+async function buildServer(dst: string, os: string | null = null, arch: string | null = null): Promise<string> {
+  const options: child.SpawnOptions = {};
   const env = Object.create( process.env );
   options.env = env;
   if(os) {
@@ -68,8 +52,7 @@ async function buildServer(dst, os, arch) {
   if(arch) {
     env['GOARCH'] = arch;
   }
-  /** @type {string[]} */
-  let cmd = ['go', 'build', '-o', dst, ServerPath];
+  let cmd: string[] = ['go', 'build', '-o', dst, ServerPath];
   if(os || arch) {
     env['CGO_ENABLED'] = '0';
     // TODO: to link statically:
@@ -78,14 +61,13 @@ async function buildServer(dst, os, arch) {
            '-a', '-installsuffix', 'cgo', '-ldflags', '-s', ServerPath];
   }
   await del([dst]);
-  await exec(['go', 'generate', ServerPath]);
+  await exec(['go', 'generate', ServerPath], options);
   return await exec(cmd, options);
 }
 
 gulp.task('server:build', () => buildServer(ServerBin));
 
-/** @type {ChildProcess} server */
-let server = null;
+let server: child.ChildProcess | null = null;
 gulp.task('server:reload', (clbk) => {
   const spawn = () => {
     try {
@@ -93,7 +75,7 @@ gulp.task('server:reload', (clbk) => {
       server.on('error', (err) => {
         log(colors.red(`Error (server): ${err}`))
       });
-      server.stderr.on('data', (data) => {
+      server.stderr!.on('data', (data) => {
         //log((`from server:\n${data}`))
       });
     } finally {
@@ -119,11 +101,11 @@ gulp.task('server:reload', (clbk) => {
 });
 
 gulp.task('server:watch', gulp.series('server:build', 'server:reload', () => {
-  return gulp.watch(['**/*.go'], {debounceDelay: 2000}, gulp.series('server:build', 'server:reload'));
+  return gulp.watch(['**/*.go'], {delay: 2000}, gulp.series('server:build', 'server:reload'));
 }));
 
 gulp.task('client:watch', gulp.series('client:build', () => {
-  return gulp.watch(['web/**/*.js'], {debounceDelay: 100}, gulp.task('client:build'));
+  return gulp.watch(['web/**/*.js'], {delay: 100}, gulp.task('client:build'));
 }));
 
 gulp.task('build', gulp.parallel('server:build', 'client:build'));
@@ -135,10 +117,10 @@ async function deploy() {
     buildServer(exe, 'linux', 'amd64'),
     buildClient()
   ]);
-  await exec(['scp', exe, 'hexe.net:/tmp/the-gear-of-seasons']);
-  await exec(['ssh', 'nodoca', 'mv /tmp/the-gear-of-seasons /opt/books/the-gear-of-seasons']);
-  await exec(['rsync', '-auz', '--delete', '-e', 'ssh', '_resources', 'nodoca:/opt/books/the-gear-of-seasons']);
-  await exec(['ssh', 'nodoca', 'docker-compose -f /opt/books/the-gear-of-seasons/docker-compose.yml restart']);
+  await exec(['scp', exe, 'hexe.net:/tmp/the-gear-of-seasons'], {});
+  await exec(['ssh', 'nodoca', 'mv /tmp/the-gear-of-seasons /opt/books/the-gear-of-seasons'], {});
+  await exec(['rsync', '-auz', '--delete', '-e', 'ssh', '_resources', 'nodoca:/opt/books/the-gear-of-seasons'], {});
+  await exec(['ssh', 'nodoca', 'docker-compose -f /opt/books/the-gear-of-seasons/docker-compose.yml restart'], {});
   await del(exe);
 }
 gulp.task('deploy', deploy);
