1bbfd9a6f98c349b225d1b162abc90c7bd766476
Kaede Fujisaki <kaede@hexe.net>
2020-10-06 01:12:02 +0900

    Upload results

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,16 @@
-import * as webpack from "webpack";
-import WriteFilePlugin from "write-file-webpack-plugin";
+import * as webpack from 'webpack'
+import WriteFilePlugin from 'write-file-webpack-plugin'
+import path from 'path'
 
 const config: webpack.Configuration = {
   context: __dirname,
   entry: {
-    'main': __dirname+'/web/omote/main.ts',
-    'admin-editor': __dirname+'/web/ura/admin-editor.ts',
+    'main': path.join(__dirname, '/web/omote/main.ts'),
+    'admin-editor': path.join(__dirname, '/web/ura/admin-editor.ts'),
   },
   mode: 'development',
   output: {
-    path: __dirname + '/_resources/static',
+    path: path.join(__dirname, '_resources/static'),
     filename: '[name].js',
     publicPath: '/static/'
   },
