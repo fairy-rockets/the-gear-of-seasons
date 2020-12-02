4405f43d48b70020466e6962fc0e6f548cc833f2
Kaede Fujisaki <kaede@hexe.net>
2020-12-02 15:50:49 +0900

    add cache layer

--- backend/src/main/kotlin/net/hexe/the_gear_of_seasons/omote/controller/EntityController.kt
+++ backend/src/main/kotlin/net/hexe/the_gear_of_seasons/omote/controller/EntityController.kt
@@ -3,23 +3,22 @@ package net.hexe.the_gear_of_seasons.omote.controller
 import io.vertx.core.CompositeFuture
 import io.vertx.ext.web.RoutingContext
 import io.vertx.kotlin.coroutines.await
-import net.hexe.the_gear_of_seasons.shelf.ShelfVerticle.EntityType
-import net.hexe.the_gear_of_seasons.shelf.ShelfVerticle.EntityType.*
+import net.hexe.the_gear_of_seasons.shelf.Cache
 
 class EntityController {
-  private suspend fun serve(ctx: RoutingContext, type: EntityType) {
+  private suspend fun serve(ctx: RoutingContext, type: Cache.Type) {
     val id = ctx.pathParam("id")
     val resp = ctx.response()
     CompositeFuture.all(listOf(resp.sendFile(""))).await()
   }
   suspend fun serveFull(ctx: RoutingContext) {
-    serve(ctx, Full)
+    serve(ctx, Cache.Type.Full)
   }
   suspend fun serveIcon(ctx: RoutingContext) {
-    serve(ctx, Icon)
+    serve(ctx, Cache.Type.Icon)
   }
   suspend fun serveMedium(ctx: RoutingContext) {
-    serve(ctx, Medium)
+    serve(ctx, Cache.Type.Medium)
   }
 
 }
--- backend/src/main/kotlin/net/hexe/the_gear_of_seasons/shelf/ShelfVerticle.kt
+++ backend/src/main/kotlin/net/hexe/the_gear_of_seasons/shelf/ShelfVerticle.kt
@@ -4,27 +4,43 @@ import io.vertx.core.eventbus.Message
 import io.vertx.core.eventbus.MessageConsumer
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.await
+import net.hexe.the_gear_of_seasons.vertx.util.IdentityCodec
 
 class ShelfVerticle : CoroutineVerticle() {
-  enum class EntityType {
-    Full,
-    Medium,
-    Icon,
-  }
-  data class FetchRequest(val id: String, val type: EntityType)
-  data class FetchResponse(val id: String, val path: String)
-  lateinit var entityReqConsumer: MessageConsumer<FetchRequest>
-  lateinit var shelf: Shelf
+  data class FetchRequest(val id: String)
+  data class FetchResponse(val entity: Entity)
+  data class ListRequest(val year: Int?)
+  data class ListResponse(val entities: List<Entity>)
+  private lateinit var fetchReqConsumer: MessageConsumer<FetchRequest>
+  private lateinit var listReqConsumer: MessageConsumer<ListRequest>
+  private lateinit var shelf: Shelf
   override suspend fun start() {
-    val config = vertx.orCreateContext.config()
     val path = config.getJsonObject("shelf")?.getString("path") ?: "_shelf"
     shelf = Shelf(vertx, path)
     shelf.init()
-    entityReqConsumer = vertx.eventBus().consumer<FetchRequest>("entity.request")
-    entityReqConsumer.handler(this::entityRequest)
+    val bus = vertx.eventBus()
+    bus.registerDefaultCodec(FetchResponse::class.java, IdentityCodec(FetchResponse::class.java))
+    bus.registerDefaultCodec(FetchRequest::class.java, IdentityCodec(FetchRequest::class.java))
+    bus.registerDefaultCodec(ListResponse::class.java, IdentityCodec(ListResponse::class.java))
+    bus.registerDefaultCodec(ListRequest::class.java, IdentityCodec(ListRequest::class.java))
+    fetchReqConsumer = bus.consumer("shelf.fetch")
+    listReqConsumer = bus.consumer("shelf.list")
+    fetchReqConsumer.handler(this::entityRequest)
+    listReqConsumer.handler(this::listRequest)
   }
   override suspend fun stop() {
-    entityReqConsumer.unregister().await()
+    fetchReqConsumer.unregister().await()
+    listReqConsumer.unregister().await()
+  }
+  private fun listRequest(msg: Message<ListRequest>) {
+    val year = msg.body().year
+    val entities = mutableListOf<Entity>()
+    for(entity in shelf.entities.values) {
+      if(year == null || entity.localTime().year == year) {
+        entities.add(entity)
+      }
+    }
+    msg.reply(ListResponse(entities))
   }
   private fun entityRequest(msg: Message<FetchRequest>) {
     val req = msg.body()
@@ -35,12 +51,15 @@ class ShelfVerticle : CoroutineVerticle() {
     when (val entity = shelf.entities[req.id]) {
       is Image -> {
         val image: Image = entity
+        msg.reply(FetchResponse(image))
       }
       is Video -> {
         val video: Video = entity
+        msg.reply(FetchResponse(video))
       }
       is Audio -> {
         val audio: Audio = entity
+        msg.reply(FetchResponse(audio))
       }
     }
   }
