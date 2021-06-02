acfda75cae92ffccb71e618e1fc7af895cf82d6b
Kaede Fujisaki <kaede@hexe.net>
2021-06-02 19:13:44 +0900

    setup web server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,21 @@
-function main() {
+import fastify from 'fastify'
 
+function main() {
+  const http = fastify({
+    logger: true
+  });
+  http.get('/', async (request, reply) => {
+    reply.type('application/json').code(200)
+    return { hello: 'world' }
+  });
+  http.listen(8888, (err, address) => {
+    if(err !== null) {
+      console.error(`Failed to start server @ ${address}`, err);
+      throw err;
+    } else {
+      console.log(`Server started @ ${address}`)
+    }
+  });
 }
 
 main();
