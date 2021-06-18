1fa835f6413b306570e5bc10856e1dd1ed3991b6
Kaede Fujisaki <kaede@hexe.net>
2021-06-18 19:09:45 +0900

    setup route

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,26 +1,55 @@
-import fastify, { FastifyInstance, RouteHandlerMethod } from 'fastify'
+import fastify, { FastifyInstance, FastifyReply, FastifyRequest, RawReplyDefaultExpression, RawRequestDefaultExpression, RawServerBase, RouteHandlerMethod } from 'fastify'
+import { RouteGenericInterface } from 'fastify/types/route';
+
+const OMOTE_HOST = process.env['OMOTE_HOST'] || 'hexe.net';
+const URA_HOST = process.env['URA_HOST'] || 'ura.hexe.net';
 
 class Server {
   private readonly http: FastifyInstance;
   private readonly both = {
-    get: (path: string, handler: RouteHandlerMethod) => {
-      this.http.get(path, handler);
-    },
-    post: (path: string, handler: RouteHandlerMethod) => {
-      this.http.post(path, handler);
+    get: <Interface extends RouteGenericInterface = RouteGenericInterface>(path: string, handler: (req: FastifyRequest<Interface>, reply: FastifyReply) => PromiseLike<void>) => {
+      this.http.get<Interface>(path, async (req, reply) => {
+        await handler(req, reply);
+      });
+    }
+  };
+  private readonly omote = {
+    get: <Interface extends RouteGenericInterface = RouteGenericInterface>(path: string, handler: (req: FastifyRequest<Interface>, reply: FastifyReply) => PromiseLike<void>) => {
+      this.http.get<Interface>(path, async (req, reply) => {
+        if(req.hostname === OMOTE_HOST) {
+          await handler(req, reply);
+        } else {
+          reply.type('text/plain').code(404);
+          reply.send('Page not found');
+        }
+      });
+    }
+  };
+  private readonly ura = {
+    get: <Interface extends RouteGenericInterface = RouteGenericInterface>(path: string, handler: (req: FastifyRequest<Interface>, reply: FastifyReply) => PromiseLike<void>) => {
+      this.http.get<Interface>(path, async (req, reply) => {
+        if(req.hostname === URA_HOST) {
+          await handler(req, reply);
+        } else {
+          reply.type('text/plain').code(404);
+          reply.send('Page not found');
+        }
+      });
     }
   };
   constructor() {
     this.http = fastify({
-      logger: true
+      logger: true,
+      bodyLimit: 256*1024*1024,
+      maxParamLength: 1024*1024,
     });
     this.setup();
   }
 
   setup() {
-    this.both.get('/', async (request, reply) => {
-      reply.type('application/json').code(200)
-      return { hello: 'world' }
+    this.both.get('/', async (req, reply) => {
+      reply.type('application/json').code(200);
+      reply.send({ hello: 'world' });
     });
   }
 
