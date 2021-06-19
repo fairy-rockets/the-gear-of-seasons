d39fbcee647097b92cabfd75a78838183a192954
Kaede Fujisaki <kaede@hexe.net>
2021-06-19 10:05:40 +0900

    separate and re-export

--- lib/src/lib.ts
+++ lib/src/lib.ts
@@ -1,4 +1,9 @@
-namespace Gear {
-  export type IndexResponse = {
-  }
-}
+import * as gear from './gear';
+import * as fml from './fml';
+import * as media from './media';
+
+export {
+  gear,
+  fml,
+  media,
+};
