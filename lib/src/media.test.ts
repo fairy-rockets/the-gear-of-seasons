94b410c8a17660e399d17fcdf29520354fa092a1
Kaede Fujisaki <kaede@hexe.net>
2021-06-19 09:18:13 +0900

    add jpeg parse test

--- lib/src/media.test.ts
+++ lib/src/media.test.ts
@@ -1,9 +1,13 @@
 // https://jestjs.io/docs/api
 import {describe, expect, it } from '@jest/globals';
 import * as media from './media';
+import * as path from 'path';
 
 describe("Media", () => {
-  it("parse jpeg", () => {
-    const path = 
+  it("parse jpeg", async () => {
+    const filepath = path.join(__dirname, '..', '..', '_assets', 'static', 'kaede.jpg');
+    const r = await media.probe(filepath);
+    expect(r.width).toEqual(1000);
+    expect(r.height).toEqual(1000);
   });
 });
--- lib/src/media.ts
+++ lib/src/media.ts
@@ -1,10 +1,20 @@
 import spawn from '@expo/spawn-async';
 
-export async function probe(path: string) {
+export type ProbeResult = {
+  codec_long_name: string,
+  codec_type: string,
+  width: number,
+  height: number,
+  duration: number,
+};
+
+export async function probe(path: string): Promise<ProbeResult> {
   const result = await spawn('ffprobe', [
     '-i', path,
-    '-print_format json',
+    '-print_format', 'json',
     '-show_streams',
   ]);
   const stdout = result.output[0];
+  const json = JSON.parse(stdout);
+  return json['streams'][0] as ProbeResult;
 }
