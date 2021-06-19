c6a659b39b45c2fa7c7f7e1ada26a1b5a2314fb0
Kaede Fujisaki <kaede@hexe.net>
2021-06-20 02:04:28 +0900

    move

--- lib/src/media.test.ts
+++ lib/src/media.test.ts
@@ -6,7 +6,7 @@ import Asset from './asset';
 
 describe("Media", () => {
   it("parse jpeg", async () => {
-    const filepath = new Asset().pathOf('static/kaede.jpg');
+    const filepath = new Asset().pathOf('static/omote/kaede.jpg');
     const r = await media.probe(filepath);
     expect(r.width).toEqual(1000);
     expect(r.height).toEqual(1000);
