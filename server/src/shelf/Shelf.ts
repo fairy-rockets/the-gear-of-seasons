f0f1651a597d1770f05a0b00f9cea4a23b9101b9
Kaede Fujisaki <kaede@hexe.net>
2021-06-22 07:38:14 +0900

    add shelf class

---
+++ server/src/shelf/Shelf.ts
@@ -0,0 +1,12 @@
+import Repo from '../repo/Repo';
+import path from 'path';
+
+class Shelf {
+  private readonly path: string;
+  private readonly repo: Repo;
+  constructor(repo: Repo) {
+    this.path = path.join(__dirname, '..', '..', '..', '_shelf');
+    this.repo = repo;
+  }
+  
+}
