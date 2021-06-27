2aeeeab0734405595168cd36ce12baa93ae30607
Kaede Fujisaki <kaede@hexe.net>
2021-06-27 16:47:40 +0900

    Fetch just first line.

--- server/src/repo/Repo.ts
+++ server/src/repo/Repo.ts
@@ -1,15 +1,14 @@
 import Pool from './Pool';
-import {Entity, ImageEntity, VideoEntity, AudioEntity} from '../shelf/Entity';
+import {Entity, ImageEntity} from '../shelf/Entity';
 import dayjs from "dayjs";
-import {Video} from "lib/dist/fml";
+import {ResultIterator, Value} from "ts-postgres";
 
 export default class Repo {
   readonly pool: Pool;
   constructor() {
     this.pool = new Pool('postgres');
   }
-  async findEntity(id: string): Promise<Entity[]> {
-    const results: Entity[] = [];
+  async findEntity(id: string): Promise<Entity | null> {
     // language=PostgreSQL
     const q1 = `
 select
@@ -24,47 +23,41 @@ select
 from entities
   where id = $1
 `;
-    const result = await this.pool.query(q1, [id]);
-    for await (const row of result) {
-      const type = row.get('type');
-      switch (type) {
-        case 'image':
-          const image: ImageEntity = {
-            id: row.get('id') as string,
-            timestamp: dayjs(row.get('timestamp') as Date),
-            mimeType: row.get('mime_type') as string,
-            type: 'image',
-            width: row.get('width') as number,
-            height: row.get('height') as number,
-          };
-          results.push(image);
-          break;
-        case 'video':
-          const video: VideoEntity = {
-            id: row.get('id') as string,
-            timestamp: dayjs(row.get('timestamp') as Date),
-            mimeType: row.get('mime_type') as string,
-            type: 'video',
-            width: row.get('width') as number,
-            height: row.get('height') as number,
-            duration: row.get('duration') as number,
-          };
-          results.push(video);
-          break;
-        case 'audio':
-          const audio: AudioEntity = {
-            id: row.get('id') as string,
-            timestamp: dayjs(row.get('timestamp') as Date),
-            mimeType: row.get('mime_type') as string,
-            type: 'audio',
-            duration: row.get('duration') as number,
-          };
-          results.push(audio);
-          break;
-        default:
-          throw new Error(`Unknown type: ${type}`)
-      }
+    const row = await this.pool.query(q1, [id]).then((it) => it.first());
+    if (row === undefined) {
+      return null;
     }
-    return results;
+    const type = row.get('type');
+    switch (type) {
+      case 'image':
+        return {
+          id: row.get('id') as string,
+          timestamp: dayjs(row.get('timestamp') as Date),
+          mimeType: row.get('mime_type') as string,
+          type: 'image',
+          width: row.get('width') as number,
+          height: row.get('height') as number,
+        };
+      case 'video':
+        return {
+          id: row.get('id') as string,
+          timestamp: dayjs(row.get('timestamp') as Date),
+          mimeType: row.get('mime_type') as string,
+          type: 'video',
+          width: row.get('width') as number,
+          height: row.get('height') as number,
+          duration: row.get('duration') as number,
+        };
+      case 'audio':
+        return {
+          id: row.get('id') as string,
+          timestamp: dayjs(row.get('timestamp') as Date),
+          mimeType: row.get('mime_type') as string,
+          type: 'audio',
+          duration: row.get('duration') as number,
+        };
+      default:
+        throw new Error(`Unknown type: ${type}`)
+    }
   }
 }
