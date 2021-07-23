814fa2158d1271f8f0916a348fc56c8c9bb3f73c
Kaede Fujisaki <kaede@hexe.net>
2021-07-23 12:34:24 +0900

    dev env

--- _helpers/cl.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo -n "$1: "
-( find "$1" -type f -print0 | xargs -0 cat ) | wc -l | sed -e 's/[ \t]//g'
--- db/helpers/wait-boot.sh
+++ db/helpers/wait-boot.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 echo -n '[Waiting Postgres] '
 export 'PGPASSWORD=the-gear-of-seasons'
-while ! psql '--username=the-gear-of-seasons' '--host=postgres' -c 'SELECT NOW();' > /dev/null  2>&1; do
+while ! psql '--username=the-gear-of-seasons' "--host=$1" -c 'SELECT NOW();' > /dev/null  2>&1; do
   echo -n '.'
   sleep 1
 done
