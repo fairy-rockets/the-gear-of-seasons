bde60027c7b6d98a2fc835032610bb862babae3f
Kaede Fujisaki <kaede@hexe.net>
2021-08-18 11:28:58 +0900

    fmt

--- server/src/Config.ts
+++ server/src/Config.ts
@@ -1,6 +1,6 @@
 const Config = {
-  OmoteHost: process.env['OMOTE_HOST'] || 'hexe.net',
-  UraHost: process.env['URA_HOST'] || 'ura.hexe.net',
+  OmoteHost:  process.env['OMOTE_HOST'] || 'hexe.net',
+  UraHost:    process.env['URA_HOST'] || 'ura.hexe.net',
   dbHostname: process.env['DB_HOST'] || 'postgres',
 };
 
