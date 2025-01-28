997872694735f85112d1be64477a34d7bd110362
Kaede Fujisaki <kaede@hexe.net>
2025-01-28 11:01:03 +0900

    Separate

---
+++ tools/restore.rs/src/app/storage.rs
@@ -0,0 +1,92 @@
+use std::collections::HashSet;
+use std::ffi::OsStr;
+use std::path::{Path, PathBuf};
+use tracing::info;
+use crate::app;
+
+pub fn walk<F>(path: &Path, mut f: F)
+  where F: FnMut(&str, PathBuf) -> anyhow::Result<()>
+{
+  let ext_jpgs = HashSet::from([
+    OsStr::new("jpg").to_os_string(),
+    OsStr::new("jpeg").to_os_string(),
+    OsStr::new("png").to_os_string(),
+  ]);
+  for entry in walkdir::WalkDir::new(path) {
+    let Ok(entry) = entry else {
+      continue;
+    };
+    if !entry.file_type().is_file() {
+      continue;
+    }
+    let path = entry.path();
+    let ext = path.extension();
+    let Some(ext) = ext else {
+      continue;
+    };
+    let ext = ext.to_ascii_lowercase();
+    if !ext_jpgs.contains(&ext) {
+      continue;
+    }
+    let content = std::fs::read(path);
+    let Ok(content) = content else {
+      tracing::warn!("Failed to read content: {}, err={:?}", path.display(), entry.file_name());
+      continue;
+    };
+    let hash = format!("{:x}", md5::compute(content));
+    tracing::info!("{}: {}", path.display(), &hash);
+    match f(&hash, path.to_path_buf()) {
+      Ok(_) => {},
+      Err(err) => {
+        tracing::error!("{}: {}", path.display(), err);
+      },
+    };
+  }
+}
+
+pub fn run(m: &clap::ArgMatches) -> anyhow::Result<()> {
+  let input = m.get_one::<String>("INPUT").expect("[BUG] No INPUT");
+  let output = m.get_one::<String>("OUTPUT").expect("[BUG] No OUTPUT");
+  let storage = m.get_one::<String>("STORAGE").expect("[BUG] No PATH");
+  info!("input: {}", input);
+  info!("output: {}", output);
+  info!("storage: {}", storage);
+
+  // Read the input hashes
+  info!("[{}] reading input file.", input);
+  let (mut images, duration) = {
+    let beg = std::time::Instant::now();
+    let content = std::fs::read_to_string(input)?;
+    let mut images = std::collections::HashSet::new();
+    for id in content.split("\n") {
+      images.insert(id.to_string());
+    }
+    let end = std::time::Instant::now();
+    (images, end.duration_since(beg))
+  };
+  info!("[{}] {} images, took {} [ms]", input, images.len(), duration.as_millis());
+
+  // Walk the storage dir.
+  std::fs::create_dir_all(output)?;
+  app::storage::walk(Path::new(storage), |hash, path| {
+    if !images.contains(hash) {
+      return Ok(());
+    }
+    let d1 = &hash[0..2];
+    let d2 = &hash[2..4];
+    let d3 = &hash[4..6];
+    let f = &hash[6..];
+    let dst = Path::new(output).join(d1).join(d2).join(d3);
+    std::fs::create_dir_all(dst.clone())?;
+    std::fs::copy(path, dst.join(f))?;
+    images.remove(hash);
+    Ok(())
+  });
+
+  // Copy to the output.
+  for hash in images {
+    info!("image with hash={} not found", hash);
+  }
+
+  Ok(())
+}
--- tools/restore.rs/src/main.rs
+++ tools/restore.rs/src/main.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
+use clap::ArgMatches;
 
-mod storage;
+mod app;
 
 const PATH: &'static str = r#"\\10.2.4.16\Public\Works\Photos"#;
 
@@ -11,31 +12,33 @@ fn app() -> clap::Command {
     .author("Kaede Fujisaki")
     .about("Frog call analyzer")
     .version("0.1.0")
-    .arg(Arg::new("verbose")
-      .long("verbose")
-      .short('v')
-      .required(false)
-      .action(ArgAction::Count)
-      .value_parser(value_parser!(u8))
-      .help("Show verbose message"))
-    .arg(Arg::new("STORAGE")
-      .long("storage")
-      .required(true)
-      .action(ArgAction::Set)
-      .value_parser(value_parser!(String))
-      .help("File origin"))
-    .arg(Arg::new("INPUT")
-      .long("input")
-      .required(true)
-      .action(ArgAction::Set)
-      .value_parser(value_parser!(String))
-      .help("Input file (md5 hash list)"))
-    .arg(Arg::new("OUTPUT")
-      .long("output")
-      .required(true)
-      .action(ArgAction::Set)
-      .value_parser(value_parser!(String))
-      .help("Output storage path"))
+    .subcommand(clap::Command::new("exact")
+      .about("exact-match by md5 hash")
+      .arg(Arg::new("verbose")
+        .long("verbose")
+        .short('v')
+        .required(false)
+        .action(ArgAction::Count)
+        .value_parser(value_parser!(u8))
+        .help("Show verbose message"))
+      .arg(Arg::new("STORAGE")
+        .long("storage")
+        .required(true)
+        .action(ArgAction::Set)
+        .value_parser(value_parser!(String))
+        .help("File origin"))
+      .arg(Arg::new("INPUT")
+        .long("input")
+        .required(true)
+        .action(ArgAction::Set)
+        .value_parser(value_parser!(String))
+        .help("Input file (md5 hash list)"))
+      .arg(Arg::new("OUTPUT")
+        .long("output")
+        .required(true)
+        .action(ArgAction::Set)
+        .value_parser(value_parser!(String))
+        .help("Output storage path")))
 
 }
 
@@ -49,50 +52,10 @@ fn main() -> anyhow::Result<()> {
     .with_writer(std::io::stderr)
     .finish()
     .init();
-  use tracing::info;
   let m = app().get_matches();
-  let input = m.get_one::<String>("INPUT").expect("[BUG] No INPUT");
-  let output = m.get_one::<String>("OUTPUT").expect("[BUG] No OUTPUT");
-  let storage = m.get_one::<String>("STORAGE").expect("[BUG] No PATH");
-  info!("input: {}", input);
-  info!("output: {}", output);
-  info!("storage: {}", storage);
-
-  // Read the input hashes
-  info!("[{}] reading input file.", input);
-  let (mut images, duration) = {
-    let beg = std::time::Instant::now();
-    let content = std::fs::read_to_string(input)?;
-    let mut images = std::collections::HashSet::new();
-    for id in content.split("\n") {
-      images.insert(id.to_string());
-    }
-    let end = std::time::Instant::now();
-    (images, end.duration_since(beg))
-  };
-  info!("[{}] {} images, took {} [ms]", input, images.len(), duration.as_millis());
-
-  // Walk the storage dir.
-  std::fs::create_dir_all(output)?;
-  storage::walk(Path::new(storage), |hash, path| {
-    if !images.contains(hash) {
-      return Ok(());
-    }
-    let d1 = &hash[0..2];
-    let d2 = &hash[2..4];
-    let d3 = &hash[4..6];
-    let f = &hash[6..];
-    let dst = Path::new(output).join(d1).join(d2).join(d3);
-    std::fs::create_dir_all(dst.clone())?;
-    std::fs::copy(path, dst.join(f))?;
-    images.remove(hash);
-    Ok(())
-  });
-
-  // Copy to the output.
-  for hash in images {
-    info!("image with hash={} not found", hash);
+  match m.subcommand() {
+    Some(("exact", m)) => app::storage::run(m),
+    Some((name, _)) => Err(anyhow::anyhow!("Unknown subcommand {}", name)),
+    None => Err(anyhow::anyhow!("No subcommand given")),
   }
-
-  Ok(())
 }
